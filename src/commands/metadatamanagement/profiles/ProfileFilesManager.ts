fe0d5c8cffc7942f5adc4ea812207edb50e061ce
Marco Dagrada <dagrada.marco@hotmail.it>
2019-07-20 00:03:15 +0200

    [Field Creation] Updating default user-set profiles with FLS

--- src/commands/metadatamanagement/profiles/ProfileFilesManager.ts
+++ src/commands/metadatamanagement/profiles/ProfileFilesManager.ts
@@ -5,6 +5,10 @@ import * as fs from 'fs'
 import * as xml2js from 'xml2js'
 import { parseBooleans } from 'xml2js/lib/processors'
 import { PathLike } from "fs"
+import SObjectFieldDefinition from "../sObjects/structures/SObjectFieldDefinition";
+import AccessType from "./structures/AccessType";
+import SObjectFieldBuilders from "../../builders/SObjectFieldBuilders";
+import SObjectFile from "../sObjects/structures/SObjectFile";
 
 export default {
 
@@ -38,12 +42,32 @@ export default {
     })
   },
 
-  writeSObjectDefinitionFile: function (fileNamePath: PathLike, stuffToWrite: any) {
+  writeProfileDefinitionFile: function (fileNamePath: PathLike, stuffToWrite: any) {
     const builder = new xml2js.Builder({ xmldec: { standalone: undefined, encoding: 'UTF-8', version: '1.0' } })
     //Probably there's a bug in the builder class
     const xml = builder.buildObject(stuffToWrite)
     //130 is the number of characters of the first two lines containing header + root object definition
     let escaped = xml.substr(0, 130) + xml.substr(130, xml.length - 130).replace(/"/g, '&quot;').replace(/'/g, '&apos;')
     fs.writeFileSync(fileNamePath.toString(), escaped, 'utf8')
+  },
+
+  updateProfilesVisibilityForField: async function (profiles: ProfileFile[], fieldsInfos: { sObject: SObjectFile, fields: [SObjectFieldDefinition] }[], accessType: AccessType) {
+    try {
+      profiles.forEach(async profileFile => {
+        const prof = await this.readProfileDefinitionFile(profileFile)
+        fieldsInfos.forEach(fieldInfo => {
+          fieldInfo.fields.forEach(fieldMeta => {
+            prof.Profile.fieldPermissions.push({
+              readable: accessType !== AccessType.none,
+              field: `${fieldInfo.sObject.label}.${fieldMeta.fullName}`,
+              editable: accessType === AccessType.edit
+            })
+          })
+        })
+        this.writeProfileDefinitionFile(path.join(profileFile.folder.toString(), profileFile.fileName), prof)
+      })
+    } catch (err) {
+      throw Error('Error updating field-level security for profiles')
+    }
   }
 }
