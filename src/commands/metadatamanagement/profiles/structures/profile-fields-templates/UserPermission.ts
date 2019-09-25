ef47a1eabace7b36b1cfd881b064de5ca9e88d90
Marco Dagrada <dagrada.marco@hotmail.it>
2019-09-25 08:33:53 +0200

    [Refactoring] UserPermissions

--- src/commands/metadatamanagement/profiles/structures/profile-fields-templates/UserPermission.ts
+++ src/commands/metadatamanagement/profiles/structures/profile-fields-templates/UserPermission.ts
@@ -1,9 +1,4 @@
-export default class UserPermission {
-    enabled: boolean
-    name: string
-
-    constructor(enabled: boolean, name: string) {
-        this.enabled = enabled
-        this.name = name
-    }
+export default interface UserPermission {
+  enabled: boolean
+  name: string
 }
